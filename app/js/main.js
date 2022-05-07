9d8bfbec63aa1924a38982fbacbd3a3aa0e58558
Alex <jobalex632@gmail.com>
2022-05-07 23:36:31 +0300

    responsive, grid, row

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,23 @@
+/* btn menu */
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header-row-min');
 
 burger.addEventListener('click', function () {
    burger.classList.toggle('active');
-   console.log(menu.classList.toggle('active'));
-})
+});
+
+/* filter cards */
+let gridBtn = document.querySelector('.grid-btn');
+let rowBtn = document.querySelector('.row-btn');
+
+gridBtn.addEventListener('click', function () {
+   $(".date-cards").last().addClass("date-cards-grid");
+});
+rowBtn.addEventListener('click', function () {
+   $(".date-cards").last().removeClass("date-cards-grid");
+});
+
+
+
+
+
